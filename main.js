d554e5ddc7ac9306cb9b7645a016e66faaca71c2
marionjames <maarionjames2018@gmail.com>
2019-09-30 00:11:39 +0300

    add a jquery function that has a toggle effect

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 //toggle effect on the "what we do section"//
 
+$(document).ready(function() {
+  $(".image1").click(function() {
+    $("#design").toggle();
+    $(".image1").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".image2").click(function() {
+    $("#development").toggle();
+    $(".image2").hide();
+  });
+});
+
+$(document)
