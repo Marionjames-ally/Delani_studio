f238999f45b6820113490193e518881bbce3959f
marionjames <maarionjames2018@gmail.com>
2019-09-30 08:50:33 +0300

    introduce the hover effect

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 //toggle effect on the "what we do section"//
 
 $(document).ready(function() {
-  $(".image1").click(function() {
+  $(".design").click(function() {
     $("#design").toggle();
-    $(".image1").hide();
+    $(".image1").toggle();
   });
 });
 
 $(document).ready(function() {
-  $(".image2").click(function() {
+  $(".development").click(function() {
     $("#development").toggle();
-    $(".image2").hide();
+    $(".image2").toggle();
   });
 });
 
-$(document)
+$(document).ready(function() {
+  $(".management").click(function() {
+    $("#management").toggle();
+    $(".image3").toggle();
+  });
+});
+
+//create a hover effect on the portfolio section showing the name of the projects//
+
+$(document).ready(function() {
+  $("#work1").hover(function() {});
+});
